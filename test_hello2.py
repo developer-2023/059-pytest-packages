2e1fc7365c93eff4f845ad007b00e23223bf231f
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-13 22:37:46 +0100

    Update test_hello2.py

--- test_hello2.py
+++ test_hello2.py
@@ -1,6 +1,8 @@
 from hello import hello
 import sys
 
+# 7:00:05 Harvard CS50’s Introduction to Programming with Python – Full University Course
+
 def test_argument():
     for name in ["Hermione", "Harry", "Ron"]:
         assert hello(name) == f"hello, {name}"
